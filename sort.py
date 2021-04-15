b4b50cf753ff213f21242c574e95f2665ac05cc7
musan6363 <msn151336+prog@gmail.com>
2021-04-16 00:34:40 +0900

    Improved for readability

--- sort.py
+++ sort.py
@@ -1,25 +1,25 @@
 import DoublyLinkedList as dll
 
 
-def insertion_sort(data):
+def insert_list_to_dll(list, has_pic=None):
+    if has_pic:
+        new_dll = dll.DoubleDataDoublyLinkedList()
+    else:
+        new_dll = dll.DoublyLinkedList()
+    # dllは値が先頭に追加されていく．
+    # listの順番を守るために，末尾から追加していく．
+    for value in reversed(list):
+        new_dll.insert(value)
+    return new_dll
+
+
+def insertion_sort(sort_target):
     """
-    >>> data1 = dll.DoublyLinkedList()
-    >>> data1.insert(3)
-    >>> data1.insert(1)
-    >>> data1.insert(6)
-    >>> data1.insert(4)
-    >>> data1.insert(2)
-    >>> data1.insert(5)
-    >>> insertion_sort(data1)
-    insertion sort
-    5 2 4 6 1 3
-    2 5 4 6 1 3
-    2 4 5 6 1 3
-    2 4 5 6 1 3
-    1 2 4 5 6 3
-    1 2 3 4 5 6
-    >>> data2 = [5,2,4,6,1,3]
-    >>> insertion_sort(data2)
+    >>> # データの準備 listはミュータブルでソート関数を通すと上書きされる．
+    >>> # 先にdllにも同じリストをコピーしておく．
+    >>> sort_target_list = [5, 2, 4, 6, 1, 3]
+    >>> sort_target_dll = insert_list_to_dll(sort_target_list)
+    >>> insertion_sort(sort_target_list)
     insertion sort
     [5, 2, 4, 6, 1, 3]
     [2, 5, 4, 6, 1, 3]
@@ -27,92 +27,110 @@ def insertion_sort(data):
     [2, 4, 5, 6, 1, 3]
     [1, 2, 4, 5, 6, 3]
     [1, 2, 3, 4, 5, 6]
+    >>> insertion_sort(sort_target_dll)
+    insertion sort
+    5 2 4 6 1 3
+    2 5 4 6 1 3
+    2 4 5 6 1 3
+    2 4 5 6 1 3
+    1 2 4 5 6 3
+    1 2 3 4 5 6
     """
     print("insertion sort")
-    if type(data) is list:
-        print(data)
-        for i in range(1, len(data)):
-            target = data[i]
-            j = i - 1
-            while j >= 0 and data[j] > target:
-                data[j+1] = data[j]
-                j -= 1
-            data[j+1] = target
-            print(data)
+
+    if type(sort_target) is list:
+        print(sort_target)
+        # "cp" は comparison_position．
+        # "tp" は target_position．
+        for cp in range(1, len(sort_target)):
+            comparison = sort_target[cp]
+            tp = cp - 1
+            while tp >= 0 and sort_target[tp] > comparison:
+                sort_target[tp+1] = sort_target[tp]
+                tp -= 1
+            sort_target[tp+1] = comparison
+            print(sort_target)
 
     else:
-        data.show()
-        target = data.head.next.next
-        while target is not data.tail:
-            sorted = target.prev
-            while sorted is not data.head and sorted.x > target.x:
+        sort_target.show()
+        ct = sort_target.head.next.next  # comparison_target
+        while ct is not sort_target.tail:
+            sorted = ct.prev
+            while sorted is not sort_target.head and sorted.value > ct.value:
                 sorted = sorted.prev
-            new_target = target.next  # 次のtargetはtargetの次．上書きされないように保持
-            # ここから 挿入処理 sortedの後ろにtargetを挿入
-            target.prev.next = target.next
-            target.next.prev = target.prev
-            target.next = sorted.next
-            target.prev = sorted
-            sorted.next.prev = target
-            sorted.next = target
-            # ここまで 挿入処理
-            target = new_target
-            data.show()
+
+            # 次のcomparison_targetはcomparison_targetの次．上書きされないように保持
+            new_target = ct.next
+
+            # sortedの後ろにcomparison_targetを挿入
+            # 改善ポイント: swap関数としてまとめたほうが良いかも．
+            ct.prev.next = ct.next
+            ct.next.prev = ct.prev
+            ct.next = sorted.next
+            ct.prev = sorted
+            sorted.next.prev = ct
+            sorted.next = ct
+
+            ct = new_target
+            sort_target.show()
 
 
-def bubble_sort(data):
+def bubble_sort(sort_target):
     """
-    >>> data1 = dll.DoublyLinkedList()
-    >>> data1.insert(1)
-    >>> data1.insert(4)
-    >>> data1.insert(2)
-    >>> data1.insert(3)
-    >>> data1.insert(5)
-    >>> count = bubble_sort(data1)
+    >>> # データの準備 listはミュータブルでソート関数を通すと上書きされる．
+    >>> # 先にdllにも同じリストをコピーしておく．
+    >>> sort_target_list = [5, 2, 4, 6, 1, 3]
+    >>> sort_target_dll = insert_list_to_dll(sort_target_list)
+    >>> replace_count = bubble_sort(sort_target_list)
     bubble sort
-    >>> data1.show()
-    1 2 3 4 5
-    >>> print(count)
-    8
-    >>> data2 = [5,2,4,6,1,3]
-    >>> count = bubble_sort(data2)
-    bubble sort
-    >>> print(data2)
+    >>> print(sort_target_list)
     [1, 2, 3, 4, 5, 6]
-    >>> print(count)
+    >>> print(replace_count)
+    9
+    >>> replace_count = bubble_sort(sort_target_dll)
+    bubble sort
+    >>> sort_target_dll.show()
+    1 2 3 4 5 6
+    >>> print(replace_count)
     9
     """
     print("bubble sort")
-    if type(data) is list:
-        count = 0
-        flag = True
-        i = 1
-        while flag:
-            flag = False
-            for j in range(len(data)-i):
-                j = len(data) - j - 1  # N-1 ~ i+1
+    if type(sort_target) is list:
+        replace_count = 0
+        exist_unsorted_pair = True
+        unsorted_top_position = 1
+        has_picture = type(sort_target[0]) is str
+        while exist_unsorted_pair:
+            exist_unsorted_pair = False
+            # "tp" は target_position
+            for tp in range(len(sort_target)-unsorted_top_position):
+                tp = len(sort_target) - tp - 1  # N-1 ~ unsorted_top_position+1
                 if (
-                    type(data[j]) is str and data[j][1] < data[j-1][1]
+                    has_picture and sort_target[tp][1] < sort_target[tp-1][1]
                 ) or (
-                    type(data[j]) is int and data[j] < data[j-1]
+                    not has_picture and sort_target[tp] < sort_target[tp-1]
                 ):
-                    tmp = data[j]
-                    data[j] = data[j-1]
-                    data[j-1] = tmp
-                    flag = True
-                    count += 1
-            i += 1
+                    tmp = sort_target[tp]
+                    sort_target[tp] = sort_target[tp-1]
+                    sort_target[tp-1] = tmp
+                    exist_unsorted_pair = True
+                    replace_count += 1
+            unsorted_top_position += 1
     else:
-        count = 0
-        flag = True
-        unsorted_top = data.head.next
-        while flag:
-            flag = False
-            target = data.tail.prev
+        replace_count = 0
+        exist_unsorted_pair = True
+        unsorted_top = sort_target.head.next
+
+        while exist_unsorted_pair:
+            exist_unsorted_pair = False
+            target = sort_target.tail.prev
             while target is not unsorted_top:
-                if target.prev.x > target.x:
-                    unsorted_top_prev = unsorted_top.prev  # 上書き防止
-                    # ここから 入替処理
+                if target.prev.value > target.value:
+                    # 未ソート部の先頭が上書きされないようにする
+                    unsorted_top_prev = unsorted_top.prev
+
+                    # a(tmp_prev) - b(tmp_next) - c(target) - d  を
+                    # a           - c           - b         - d  に入れ替える
                     tmp_prev = target.prev.prev
                     tmp_next = target.prev
                     target.prev.prev.next = target
@@ -121,71 +139,74 @@ def bubble_sort(data):
                     target.next.prev = target.prev
                     target.prev = tmp_prev
                     target.next = tmp_next
-                    # ここまで 入替処理
+
                     unsorted_top = unsorted_top_prev.next
-                    flag = True
-                    count += 1
+
+                    exist_unsorted_pair = True
+                    replace_count += 1
                 else:
                     target = target.prev
             unsorted_top = unsorted_top.next
-    return count
+    return replace_count
 
 
-def selection_sort(data):
+def selection_sort(sort_target):
     """
-    >>> data1 = dll.DoublyLinkedList()
-    >>> data1.insert(3)
-    >>> data1.insert(1)
-    >>> data1.insert(2)
-    >>> data1.insert(4)
-    >>> data1.insert(6)
-    >>> data1.insert(5)
-    >>> count = selection_sort(data1)
+    >>> # データの準備 listはミュータブルでソート関数を通すと上書きされる．
+    >>> # 先にdllにも同じリストをコピーしておく．
+    >>> sort_target_list = [5, 2, 4, 6, 1, 3]
+    >>> sort_target_dll = insert_list_to_dll(sort_target_list)
+    >>> replace_count = selection_sort(sort_target_list)
     selection sort
-    >>> data1.show()
-    1 2 3 4 5 6
-    >>> print(count)
-    4
-    >>> data2 = [5,6,4,2,1,3]
-    >>> count = selection_sort(data2)
-    selection sort
-    >>> print(data2)
+    >>> print(sort_target_list)
     [1, 2, 3, 4, 5, 6]
-    >>> print(count)
-    4
+    >>> print(replace_count)
+    3
+    >>> replace_count = selection_sort(sort_target_dll)
+    selection sort
+    >>> sort_target_dll.show()
+    1 2 3 4 5 6
+    >>> print(replace_count)
+    3
+
     """
     print("selection sort")
-    if type(data) is list:
-        count = 0
-        for i in range(len(data)):
+    if type(sort_target) is list:
+        replace_count = 0
+        has_picture = type(sort_target[0]) is str
+        for unsorted_top_position in range(len(sort_target)):
             count_flag = False
-            minj = i
-            for j in range(i, len(data)):
+            tmp_min = unsorted_top_position
+            for i in range(unsorted_top_position, len(sort_target)):
                 if (
-                    type(data[j]) is str and data[j][1] < data[minj][1]
+                    has_picture and sort_target[i][1] < sort_target[tmp_min][1]
                 ) or (
-                    type(data[j]) is int and data[j] < data[minj]
+                    not has_picture and sort_target[i] < sort_target[tmp_min]
                 ):
-                    minj = j
+                    tmp_min = i
                     count_flag = True
-            tmp = data[i]
-            data[i] = data[minj]
-            data[minj] = tmp
+
+            tmp = sort_target[unsorted_top_position]
+            sort_target[unsorted_top_position] = sort_target[tmp_min]
+            sort_target[tmp_min] = tmp
+
             if count_flag:
-                count += 1
+                replace_count += 1
     else:
-        count = 0
-        unsorted_top = data.head.next
-        while unsorted_top is not data.tail:
+        replace_count = 0
+        unsorted_top = sort_target.head.next
+        while unsorted_top is not sort_target.tail:
             count_flag = False
             min = unsorted_top
             target = unsorted_top.next
-            while target is not data.tail:
-                if target.x < min.x:
+            while target is not sort_target.tail:
+                if target.value < min.value:
                     min = target
                     count_flag = True
                 target = target.next
-            # ここから minとunsorted_topの交換
+
+            # a(unsorted_top) - b - c(tmp_prev) - d(min) - e(tmp_next)  を
+            # d(unsorted_top) - b - c           - a      - e            にする
             tmp_prev = min.prev
             tmp_next = min.next
             unsorted_top.prev.next = min
@@ -196,46 +217,26 @@ def selection_sort(data):
             tmp_next.prev = unsorted_top
             unsorted_top.prev = tmp_prev
             unsorted_top.next = tmp_next
+
+            # minとunsorted_topが隣り合っていたときの対策
+            # min.nextがmin自身，unsorted_top.prevも自身を指している．
             if min.next is min:
-                # minとunsorted_topが隣り合っていたときの対策
                 min.next = unsorted_top
                 unsorted_top.prev = min
-            # ここまで minとunsorted_topの交換
+
             unsorted_top = min.next
             if count_flag:
-                count += 1
-    return count
+                replace_count += 1
+    return replace_count
 
 
-def stable_sort(data):
+def stable_sort(sort_target):
     """
-    >>> data1 = dll.DoubleDataDoublyLinkedList()
-    >>> data1.insert("C3")
-    >>> data1.insert("D2")
-    >>> data1.insert("S4")
-    >>> data1.insert("C9")
-    >>> data1.insert("H4")
-    >>> stable_sort(data1)
-    stable sort
-    bubble sort
-    D2 C3 H4 S4 C9
-    Stable
-    selection sort
-    D2 C3 S4 H4 C9
-    Not stable
-    >>> data2 = dll.DoubleDataDoublyLinkedList()
-    >>> data2.insert("H1")
-    >>> data2.insert("S1")
-    >>> stable_sort(data2)
-    stable sort
-    bubble sort
-    S1 H1
-    Stable
-    selection sort
-    S1 H1
-    Stable
-    >>> data3 = ['H4', 'C9', 'S4', 'D2', 'C3']
-    >>> stable_sort(data3)
+    >>> # データの準備 listはミュータブルでソート関数を通すと上書きされる．
+    >>> # 先にdllにも同じリストをコピーしておく．
+    >>> sort_target_list = ['H4', 'C9', 'S4', 'D2', 'C3']
+    >>> sort_target_dll = insert_list_to_dll(sort_target_list, has_pic=True)
+    >>> stable_sort(sort_target_list)
     stable sort
     bubble sort
     ['D2', 'C3', 'H4', 'S4', 'C9']
@@ -243,37 +244,50 @@ def stable_sort(data):
     selection sort
     ['D2', 'C3', 'S4', 'H4', 'C9']
     Not stable
+    >>> stable_sort(sort_target_dll)
+    stable sort
+    bubble sort
+    D2 C3 H4 S4 C9
+    Stable
+    selection sort
+    D2 C3 S4 H4 C9
+    Not stable
     """
     print("stable sort")
-    is_dll = type(data) is dll.DoubleDataDoublyLinkedList
-    bubble = data.copy()
-    bubble_sort(bubble)
-    if is_dll:
-        bubble.show()
+    is_dddll = type(sort_target) is dll.DoubleDataDoublyLinkedList
+
+    bubble_sort_target = sort_target.copy()
+    bubble_sort(bubble_sort_target)
+    if is_dddll:
+        bubble_sort_target.show()
     else:
-        print(bubble)
+        print(bubble_sort_target)
+
     print("Stable")  # Bubble sort is always stable
-    selection = data.copy()
-    selection_sort(selection)
-    if is_dll:
-        selection.show()
+
+    selection_sort_target = sort_target.copy()
+    selection_sort(selection_sort_target)
+    if is_dddll:
+        selection_sort_target.show()
     else:
-        print(selection)
-    if bubble == selection:
-        print("Stable")
-    elif is_dll:
-        bubble_data = bubble.head.next
-        selection_data = selection.head.next
-        while bubble_data is not bubble.tail:
+        print(selection_sort_target)
+
+    if is_dddll:
+        # bubble sort と selection sortの結果をheadから順に比較
+        bubble_item = bubble_sort_target.head.next
+        selection_item = selection_sort_target.head.next
+        while bubble_item is not bubble_sort_target.tail:
             if (
-                bubble_data.x != selection_data.x
+                bubble_item.value != selection_item.value
                 or
-                bubble_data.picture != selection_data.picture
+                bubble_item.picture != selection_item.picture
             ):
                 print("Not stable")
                 return
-            bubble_data = bubble_data.next
-            selection_data = selection_data.next
+            bubble_item = bubble_item.next
+            selection_item = selection_item.next
+        print("Stable")
+    elif bubble_sort_target == selection_sort_target:
         print("Stable")
     else:
         print("Not stable")
